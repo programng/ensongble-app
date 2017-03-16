25ec1d9515e52b8ee7be5c75c77588f88c010897
Jonathan Ng <jon7ng@gmail.com>
2017-03-16 10:02:26 -0700

    add mongoose, d3 and background

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as d3 from 'd3';
 
 function Landing() {
   return (
     <div className="landing">
-      <Link to="/about">About</Link>
+      <Link to="/about">About {d3.version}</Link>
       <h1 className="landing-name">MOVIE TUNES</h1>
     </div>
   );
