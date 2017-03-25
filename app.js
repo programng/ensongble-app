aa57105f15d2dbd20e640bab335715590bd444c5
Jonathan Ng <jon7ng@gmail.com>
2017-03-25 14:14:06 -0700

    add python script endpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const spawn = require('child_process').spawn;
 
 const app = express();
 const port = process.env.PORT || 8080;;
@@ -6,12 +7,28 @@ const port = process.env.PORT || 8080;;
 app.use(express.static(__dirname + "/dist/"));
 
 app.get('/helloworld', function (req, res) {
-  res.send('Hello World!')
-})
+  const py = spawn('python', ['test.py']);
+  let result;
 
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
+  const data = [1,2,3,4,5,6,7,8,9];
+
+  py.stdout.on('data', (data) => {
+    console.log('data', data);
+    result = data.toString();
+    console.log('result', result);
+  });
+  py.stdout.on('end', () => {
+    console.log('end python script');
+    res.send(result)
+  });
+  py.stdin.write(JSON.stringify(data));
+  py.stdin.end();
+
 });
+
+// app.post('/', function (req, res) {
+//   res.send('Got a POST request')
+// });
 
 app.listen(port, (err) => {
   if (err) {
