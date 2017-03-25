5901e0b4680268bd337ee20684dc0793722adf4b
Jonathan Ng <jon7ng@gmail.com>
2017-03-25 13:56:34 -0700

    test endpoints

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 8080;;
 
 app.use(express.static(__dirname + "/dist/"));
 
+app.get('/helloworld', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+});
+
 app.listen(port, (err) => {
   if (err) {
     return console.log('error: ', err)
